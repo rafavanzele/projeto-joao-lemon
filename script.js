6ee11b727888dbab2b1abb3b4ad56bbeb64c3f0d
rafavanzele <rafa_vanzele@hotmail.com>
2023-07-07 17:29:26 -0300

    pagina preloader finalizada

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+//DECLARANDO VARIÁVEIS
 var btnContato = document.querySelector('.jl-botao-contato');
 
+
+//PÁGINA PRELOADER
+window.addEventListener('load', function() {
+    var pagePreloader = document.querySelector('.jl-preloader');
+    pagePreloader.classList.add('jl-fade-out');
+
+    setTimeout(function() {
+        pagePreloader.style.display = 'none'
+    }, 2000);
+});
+
+
+
+//ABRINDO E FECHANDO INFORMAÇÕES DE CONTATO
 btnContato.addEventListener('click', function() {
     var boxContato = document.querySelector('.jl-contato-info')
 
