96951281149199f3f58a2f61dfe401ca8771efa6
rafavanzele <rafa_vanzele@hotmail.com>
2023-08-30 18:48:52 -0300

    js da galeria manage it

--- script.js
+++ script.js
@@ -251,3 +251,25 @@ var changeActive = function() {
     counterRemove();
     changeActive();
   });
+
+
+  //GALERIA - MANAGE IT
+
+var overlay = document.querySelector('.jl-overlay');
+var frameImage = document.querySelector('.jl-galeria-frame-image');
+var frameContainer = document.querySelector('.jl-galeria-frame-container');
+var galeriaImages = document.querySelectorAll('.jl-thumb-img');
+
+const getImageSrc = function() {
+  for (var i=0; i < galeriaImages.length; i++) {
+      galeriaImages[i].addEventListener('click', function() {
+        var imageSrc = this.getAttribute('data-src');
+        frameImage.setAttribute('src', imageSrc);
+
+        overlay.classList.add('jl-aberto');
+        frameContainer.classList.add('jl-aberto');
+        }); 
+    }
+  }
+
+  getImageSrc();
