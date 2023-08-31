6b2dc9b21ac9c0552273aa6a4348f32ebda69d25
rafavanzele <rafa_vanzele@hotmail.com>
2023-08-31 17:46:48 -0300

    toggle galeria manage it atualizado

--- galeria.js
+++ galeria.js
@@ -4,6 +4,7 @@ var overlay = document.querySelector('.jl-overlay');
 var frameImage = document.querySelector('.jl-galeria-frame-image');
 var frameContainer = document.querySelector('.jl-galeria-frame-container');
 var galeriaImages = document.querySelectorAll('.jl-thumb-img');
+var closeGaleria = document.querySelectorAll('.jl-toggle-galeria');
 
 const getImageSrc = function() {
   for (var i=0; i < galeriaImages.length; i++) {
@@ -15,6 +16,16 @@ const getImageSrc = function() {
             frameContainer.classList.add('jl-aberto');
         }); 
     }
-  }
+}
 
-  getImageSrc();
+
+for (var c=0; c < closeGaleria.length; c++) {
+    closeGaleria[c].addEventListener('click', function() {
+        overlay.classList.remove('jl-aberto');
+        frameContainer.classList.remove('jl-aberto');  
+    })
+}
+
+
+
+getImageSrc();
