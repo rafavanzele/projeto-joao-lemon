e210d870188ee6bcee3104a8b8bc7013938e81b0
rafavanzele <rafa_vanzele@hotmail.com>
2023-08-31 17:16:28 -0300

    ajustando css da galeria manage it

--- galeria.js
+++ galeria.js
@@ -7,12 +7,12 @@ var galeriaImages = document.querySelectorAll('.jl-thumb-img');
 
 const getImageSrc = function() {
   for (var i=0; i < galeriaImages.length; i++) {
-      galeriaImages[i].addEventListener('click', function() {
-        var imageSrc = this.getAttribute('data-src');
-        frameImage.setAttribute('src', imageSrc);
+        galeriaImages[i].addEventListener('click', function() {
+            var imageSrc = this.getAttribute('data-src');
+            frameImage.setAttribute('src', imageSrc);
 
-        overlay.classList.add('jl-aberto');
-        frameContainer.classList.add('jl-aberto');
+            overlay.classList.add('jl-aberto');
+            frameContainer.classList.add('jl-aberto');
         }); 
     }
   }
