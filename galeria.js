9bed921e8842335ae3ee4e68af31a88a91f05c72
rafavanzele <rafa_vanzele@hotmail.com>
2023-09-04 18:44:09 -0300

    js galeria atualizado

--- galeria.js
+++ galeria.js
@@ -5,12 +5,16 @@ var frameImage = document.querySelector('.jl-galeria-frame-image');
 var frameContainer = document.querySelector('.jl-galeria-frame-container');
 var galeriaImages = document.querySelectorAll('.jl-thumb-img');
 var closeGaleria = document.querySelectorAll('.jl-toggle-galeria');
+var btnNext = document.querySelector('.jl-item-next');
+var btnPrev = document.querySelector('.jl-item-prev');
 
 const getImageSrc = function() {
   for (var i=0; i < galeriaImages.length; i++) {
         galeriaImages[i].addEventListener('click', function() {
             var imageSrc = this.getAttribute('data-src');
+            var itemNum = this.getAttribute('data-item');
             frameImage.setAttribute('src', imageSrc);
+            frameImage.setAttribute('data-index', itemNum);
 
             overlay.classList.add('jl-aberto');
             frameContainer.classList.add('jl-aberto');
@@ -28,15 +32,33 @@ for (var c=0; c < closeGaleria.length; c++) {
 }
 
 
-//1-Identificamos o item atual no frame
 
-//2-Definir o numero do proximo item
+//CRIANDO A LÓGICA PARA BOTÃO NEXT E PREV DA GALERIA
 
-//3-Fazer o loop e identificar qual item faz match com o numero do próximo item
+const nextItem = function() {
+    //1-Identificar o item atual no frame
+    var currItemNum = frameImage.getAttribute('data-index');
+   
+    //2-Definir o numero do proximo item
+    var nextItemNum = parseInt(currItemNum) +1;
+    
+    //3-Fazer o loop e identificar qual item faz match com o numero do próximo item
+    for (var n=0; n < galeriaImages.length; n++) {
+        var item = galeriaImages[n];
+        var itemNum = parseInt(item.getAttribute('data-item'));
 
-//4-Capturar o data-src
+        if (itemNum === nextItemNum) {
+            //4-Capturar o data-src
+            var nextSrc = item.getAttribute('data-src');
+            var nextIndex = item.getAttribute('data-item');
 
-//5-Passar o data-src para a tag de img no frame
+            //5-Passar o data-src para a tag de img no frame
+            frameImage.setAttribute('src', nextSrc);
+            frameImage.setAttribute('data-index', nextIndex);
+        }
+    }
+}
 
-
-
+btnNext.addEventListener('click', function() {
+    nextItem();
+});
